64ffc6017bf40429811fb71c42a177e689213375
wobbly <robb.whiting@gmail.com>
2019-02-07 05:58:01 -0800

    Update the intro.h to include the stuff from the vs version, but removed makefile because there are some problems there.

--- linux/a_game/intro.h
+++ linux/a_game/intro.h
@@ -1,38 +1,59 @@
+#pragma once
+#include <iostream>
 #include <string>
-#include <sstream>
-#include "quit.h"
-#include <stdlib.h>
 #include <thread>
 #include <chrono>
+#include <sstream>
+#include "quit.h"
 
 using namespace std;
 
-void intro(){
-
-	system ("clear");
+void intro()
+{
+	system("clear");
 	cout << "Welcome to A Game!\n";
-	cout << "Would you like to play A Game?  ";
+	cout << "Would you like to play A Game?\n";
 
 	string answer = "";
 
-	getline (cin, answer);
+	getline(cin, answer);
 
-	if (answer != "no" && answer != "n"){
-		system ("clear");
+	if (answer != "no" && answer != "n")
+	{
+		system("clear");
 		cout << "That doesn't seem like a no!\n";
-		std::this_thread::sleep_for (std::chrono::seconds(1));
-		system ("clear");
-		cout << "Loading the first room. \n";
-		std::this_thread::sleep_for (std::chrono::seconds(1));
-		system ("clear");
-		cout << "Loading the first room. . \n";
-		std::this_thread::sleep_for (std::chrono::seconds(1));
-		system ("clear");
-		cout << "Loading the first room. . . \n";
-		std::this_thread::sleep_for (std::chrono::seconds(1));
-		cout << " \n";
-		system ("clear");
+		this_thread::sleep_for(chrono::seconds(1));
+		system("clear");
+
+		cout << "Loading the first room.\n";
+		this_thread::sleep_for(chrono::seconds(1));
+		system("clear");
+
+		cout << "Loading the first room. .\n";
+		this_thread::sleep_for(chrono::seconds(1));
+		system("clear");
+
+		cout << "Loading the first room. . .\n";
+		this_thread::sleep_for(chrono::seconds(1));
+		system("clear");
 	}
 	else
-		quit();
+	{
+		cout << "Would you like to make levels for A Game?\n";
+		string answer = "";
+
+		getline(cin, answer);
+
+		if (answer != "no" && answer != "n")
+		{
+			system("clear");
+			cout << "That doesn't seem like a no!\n";
+			this_thread::sleep_for(chrono::seconds(1));
+			system("clear");
+		}
+		else
+			//temporary
+			quit();
+			//real function here will start the game
+	}
 }
